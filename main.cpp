2bd386a9162c7ad3aa74021ba1634fd669eeb24d
ranepahe <rararanelagh@gmail.com>
2016-05-31 03:26:18 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream> //Inclut la bibliothèque iostream (affichage de texte)
+#include <string>
+
+using namespace std; //Indique quel espace de noms on va utiliser
+
+/*
+Fonction principale "main"
+Tous les programmes commencent par la fonction main
+*/
+int main()
+{
+    int qiUtilisateur(153);
+        string nomUtilisateur("Rane Pahe");
+
+            cout << "Vous vous appelez " << nomUtilisateur << " et votre QI est de " <<qiUtilisateur << endl;
+    return 0; //Termine la fonction main et donc le programme
+}
